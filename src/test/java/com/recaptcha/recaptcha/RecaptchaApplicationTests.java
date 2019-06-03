c7879af11e0425106e2a1809a7049c57bfcea8e0
Zhi Li <zhi.li@invoicefair.com>
2019-06-03 10:51:31 +0100

    init

---
+++ src/test/java/com/recaptcha/recaptcha/RecaptchaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.recaptcha.recaptcha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecaptchaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
